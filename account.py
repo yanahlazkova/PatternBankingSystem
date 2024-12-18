c1564e2d101e10a510d5d1b0df8ca903fc7f0ecb
Programmer <Programmer@ua>
2024-12-18 16:59:04 +0200

    patterns Factory for classes Account and Client

--- account.py
+++ account.py
@@ -1,33 +1,35 @@
 from abc import ABC, abstractmethod
 from faker import Faker
 
-faker = Faker()
+fake = Faker()
+
 
 class AbstractAccount(ABC):
-    account_type: str = None
-    account_number: int = None
-    balance: float = 0.0
-    owner_id: str = None
-    interest_rate: float = None
+    def __init__(self):
+        self.__account_type: str = ''
+        self.__account_number: str = ''
+        self.__balance: float = 0.0
+        self.__owner_id: str = ''
+        self.__interest_rate: float = 0.0
 
     @abstractmethod
     def set_account_type(self):
         pass
 
-    def get_account_type(self):
-        return self.account_type
+    # def get_account_type(self):
+    #     return self.__account_type
 
     def set_account_number(self, account_number):
-        self.account_number = account_number
+        self.__account_number = account_number
 
-    def set_owner(self, owner_id):
-        self.owner_id = owner_id
+    def set_owner_id(self, owner_id):
+        self.__owner_id = owner_id
 
-    def get_owner(self):
-        return self.owner_id
+    # def get_owner_id(self):
+    #     return self.__owner_id
 
     def set_interest_rate(self, interest_rate):
-        self.interest_rate = interest_rate
+        self.__interest_rate = interest_rate
 
     @abstractmethod
     def set_balance(self, balance):
@@ -43,36 +45,44 @@ class AbstractAccount(ABC):
         """ Метод зняття коштів з рахунку """
         pass
 
+    @abstractmethod
+    def interest_accrual(self):
+        """ метод нарахування відсотків"""
+        pass
+
     def __str__(self):
         return (f'Client ID: {self.owner_id}\n'
-                f'Account: "{self.account_type}" - "{self.account_number}"\n'
+                f'Account: "{self._account_type}" - "{self.account_number}"\n'
                 f'balance: {self.balance} $')
 
 
 class SavingsAccount(AbstractAccount):
     def set_account_type(self):
-        self.account_type = 'savings'
-
-    def set_interest_rate(self, interest_rate):
-        self.interest_rate = interest_rate
+        self.__account_type = 'savings'
 
     def set_balance(self, balance):
-        self.balance += self.balance
-        return self.balance
+        self.__balance += self.__balance
+        return self.__balance
 
     def deposit(self, amount):
         self.set_balance(amount)
 
     def withdraw(self, amount):
         self.balance -= amount if self.balance - amount else f'Не достатньо коштів на рахунку'
+
+    def interest_accrual(self):
+        interest = self.interest_rate * self.balance / 100
+        self.balance += interest
+        print(f'Balance is {self.balance}')
 
 
 class DepositAccount(AbstractAccount):
+    def __init__(self, fixed_period_time=None):
+        self.fixed_period_time = fixed_period_time # фіксований період часу в місяцях
+        self.interest_penalty = 5 # відсоток штрафу при знятті коштів
+
     def set_account_type(self):
         self.account_type = 'deposit'
-
-    def get_account_type(self):
-        return self.account_type
 
     def set_balance(self, balance):
         return self.balance
@@ -81,15 +91,42 @@ class DepositAccount(AbstractAccount):
         self.set_balance(amount)
 
     def withdraw(self, amount):
-        self.balance -= amount if self.balance - amount else f'Не достатньо коштів на рахунку'
+        """ Заборона зняття коштів до закінчення депозитного періоду,
+        або можливість зняття зі штрафом """
+        # Логіка: 1) Якщо знімається не вся сумма до закінчення періоду - заборона зняття
+        # 2) Вся сумма - вирахувати штраф. Перевірити чи достатньо коштів для зняття зі штрафом
+        if self.fixed_period_time > 0:
+            if amount < self.balance:
+                # заборона зняття
+                print(f'Не можливо зняти {amount} $ до закінчення депозитного періоду')
+                return
+            if amount == self.__balance:
+                penalty = self.penalty_accrual()
+                amount_withdraw = self.balance - penalty
+
+                print(f'Нараховано штраф {penalty} $, Сума до видачі: {self.balance - penalty} $')
+                self.set_balance(-self.balance)
+
+    def penalty_accrual(self):
+        """ нарахування штрафу"""
+        penalty = self.balance * self.interest_penalty / 100
+        return penalty
+
+    def interest_accrual(self):
+        """ Має можливість накопичувати відсотки протягом фіксованого періоду часу (в місяцях)
+        Період часу при нарахуванні відсотків буде зменьшуватись на 1 місяць """
+        if self.fixed_period_time > 0:
+            interest = self.interest_rate * self.balance / 100
+            self.balance += interest
+            self.fixed_period_time -= 1
+            print(f"Відсотки нараховані ({interest} $). На рахунку {self.balance}")
+        else:
+            print("Термін накопичення відсотків закінчився")
 
 
 class CreditAccount(AbstractAccount):
     def set_account_type(self):
         self.account_type = 'credit'
-
-    def get_account_type(self):
-        return self.account_type
 
     def get_owner(self):
         return self.owner_id
@@ -106,11 +143,12 @@ class CreditAccount(AbstractAccount):
 
 class AccountFactory:
     @staticmethod
-    def create_account(account_type) -> AbstractAccount:
+    def create_account(account_type, account_data) -> AbstractAccount:
         if account_type == 'savings':
             account = SavingsAccount()
             account.set_account_type()
-            account.set_account_number(faker.random_number(27))
+            account.set_account_number(fake.random_number(27))
+            return account
         elif account_type == 'deposit':
             return DepositAccount()
         elif account_type == 'credit':
@@ -119,7 +157,7 @@ class AccountFactory:
             raise ValueError("Un account type")
 
     @staticmethod
-    def deposit(self, account: DepositAccount, amount: float):
+    def deposit(self, account, amount):
         account.balance += amount
         print()
         return f'Your balance = {self.balance}'
--- client.py
+++ client.py
@@ -26,13 +26,13 @@ class Client(AbstractPerson):
         return self.client_id()
 
     def get_name(self):
-        return self.name()
+        return self.name
 
     def add_account(self, number_account):
         self.list_accounts.append(number_account)
 
     def get_list_accounts(self):
-        return "\n".join(str(account) for account in self.list_accounts)
+        return "\n".join(f'\t{index + 1} {str(account)}' for index, account in enumerate(self.list_accounts))
 
     def __str__(self):
         return (f'id: {self.client_id}\tname: {self.name}\n'
@@ -40,26 +40,24 @@ class Client(AbstractPerson):
                 f'{self.get_list_accounts()}')
 
 
-
-from faker import Faker
-fake = Faker()
-
-
 class ClientFactory:
     @staticmethod
-    def create_client():
-        client = Client(fake.name(), str(uuid.uuid4()))
-
+    def create_client(name, client_id):
+        client = Client(name=name, client_id=client_id)
         return client
 
 
-for _ in range(5):
-    new_client = ClientFactory.create_client()
-    client_account = 'UA' + str(fake.random_number(27))
-    new_client.add_account(client_account)
-    print(new_client)
 
-new_account = 'UA' + str(fake.random_number(27))
-new_client.add_account(new_account)
-
-print(new_client)
+# from faker import Faker
+# fake = Faker()
+#
+# for index in range(5):
+#     new_client = ClientFactory.create_client(fake.name(), str(uuid.uuid4()))
+#     client_account = 'UA' + str(fake.random_number(27))
+#     new_client.add_account(client_account)
+#     print(f'{index + 1}.', new_client)
+#
+# new_account = 'UA' + str(fake.random_number(27))
+# new_client.add_account(new_account)
+#
+# print(new_client)
